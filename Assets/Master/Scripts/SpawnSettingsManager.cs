e032ce063066112df3d2b52704846f78a16f4124
someone <you@example.com>
2017-05-03 23:39:44 -0500

    spawning

---
+++ Assets/Master/Scripts/SpawnSettingsManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SpawnSettingsManager : NetworkBehaviour {
+
+    public GameObject settings;
+	// Use this for initialization
+	void Start () {
+        NetworkServer.Spawn(settings);
+        Debug.Log("spawned");
+	}
+	
+	//// Update is called once per frame
+	//void Update () {
+		
+	//}
+}

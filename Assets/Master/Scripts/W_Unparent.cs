1513a92f86841a075f691db499a1503bf350ea35
david lobser <davidlobser@yahoo.com>
2017-09-26 19:55:45 -0400

    added flower wig

---
+++ Assets/Master/Scripts/W_Unparent.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ONTempleATrip { 
+    public class W_Unparent : MonoBehaviour {
+
+	    // Use this for initialization
+	    void Start () {
+            this.transform.parent = null;
+            this.transform.position = Vector3.zero;
+            this.transform.localEulerAngles = Vector3.zero;
+	    }
+	
+    }
+}

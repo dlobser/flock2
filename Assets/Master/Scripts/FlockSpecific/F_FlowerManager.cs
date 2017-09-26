3f306bdce4bdcd36b4b5cd547ee3dcef3ad729de
david lobser <davidlobser@yahoo.com>
2017-09-26 16:54:00 -0400

    flower manager

--- Assets/Master/Scripts/FlockSpecific/F_FlowerManager.cs
+++ Assets/Master/Scripts/FlockSpecific/F_FlowerManager.cs
@@ -12,6 +12,7 @@ public class F_FlowerManager : NetworkBehaviour {
 	public float searchFrequency;
 	public GameObject networkObject;
 	public SettingsManager2 settings;
+	bool doSearch = true;
 
 	void Start () {
 		trackedObjects = new GameObject[trackedObjectNames.Length];
@@ -19,7 +20,15 @@ public class F_FlowerManager : NetworkBehaviour {
 		StartCoroutine (search ());
 	}
 
-	IEnumerator search(){
+	void Update(){
+		if (doSearch) {
+			finder ();
+			doSearch = false;
+			StartCoroutine (search ());
+		}
+	}
+
+	void finder(){
 		for (int i = 0; i < trackedObjectNames.Length; i++) {
 			GameObject found = GameObject.Find (trackedObjectNames [i]);
 			if (found == null && trackedObjects [i] != null) {
@@ -28,23 +37,23 @@ public class F_FlowerManager : NetworkBehaviour {
 			} else if (found != null && trackedObjects [i] == null) {
 				trackedObjects [i] = found;
 			}
-//			if (found!=null && trackedObjects [i] != null && networkedObjects [i] == null) {
-//				if (NetworkServer.active) {// || NetworkClient.active) {
-//                    InstanceNetworkObject(i, 
-//                        networkObject,
-//                        found);
-//                    //Debug.Log(g);
-//                    Debug.Log("networrrrkkkk");
-//
-//                    //NetworkServer.SpawnWithClientAuthority( networkedObjects [i],g);
-//
-//                    //NetworkServer.Spawn(networkedObjects[i]);
-//                    //networkedObjects [i].GetComponent<F_CopyXForms> ().target = found.transform;
-//				}
-//			}
+			//			if (found!=null && trackedObjects [i] != null && networkedObjects [i] == null) {
+			//				if (NetworkServer.active) {// || NetworkClient.active) {
+			//                    InstanceNetworkObject(i, 
+			//                        networkObject,
+			//                        found);
+			//                    //Debug.Log(g);
+			//                    Debug.Log("networrrrkkkk");
+			//
+			//                    //NetworkServer.SpawnWithClientAuthority( networkedObjects [i],g);
+			//
+			//                    //NetworkServer.Spawn(networkedObjects[i]);
+			//                    //networkedObjects [i].GetComponent<F_CopyXForms> ().target = found.transform;
+			//				}
+			//			}
 			if (found!=null && trackedObjects [i] != null && networkedObjects [i] == null) {
 				if (NetworkClient.active) {// || NetworkClient.active) {
-                    Debug.Log("networkclient" + NetworkClient.active);
+					Debug.Log("networkclient" + NetworkClient.active);
 					CmdInstanceNetworkObject(i, 
 						networkObject,
 						found);
@@ -58,13 +67,18 @@ public class F_FlowerManager : NetworkBehaviour {
 				}
 			}
 		}
+	}
+
+	IEnumerator search(){
+		
 		yield return new WaitForSeconds (searchFrequency);
-		StartCoroutine (search ());
+		doSearch = true;
 	}
 
     [Command]
     public void CmdInstanceNetworkObject(int which, GameObject toInstance, GameObject found) {
         Debug.Log(this.connectionToClient);
+		Debug.Log ("Hi There");
 //        GameObject g = GameObject.FindObjectOfType<F_IsLocalPlayer>().gameObject;
         networkedObjects[which] = (GameObject)Instantiate(networkObject, found.transform.position, found.transform.rotation);
 		NetworkServer.SpawnWithClientAuthority( networkedObjects [which],this.connectionToClient);

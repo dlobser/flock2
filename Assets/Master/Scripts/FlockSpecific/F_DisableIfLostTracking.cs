4df7149666f10784d0db4eec56e56c6f931a840c
david lobser <davidlobser@yahoo.com>
2017-09-28 12:06:53 -0400

    imported tango

---
+++ Assets/Master/Scripts/FlockSpecific/F_DisableIfLostTracking.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class F_DisableIfLostTracking : MonoBehaviour {
+
+	public F_CopyXForms xForms;
+	// Use this for initialization
+	void Start () {
+		xForms = this.GetComponent<F_CopyXForms> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (xForms.target == null) {
+			this.gameObject.SetActive (false);
+		}
+	}
+}
